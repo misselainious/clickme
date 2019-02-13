4949659d4caedba684c1001d620f5da69c7e5f31
Elaine <elaine.cadman@gmail.com>
2019-02-13 14:13:49 -0800

    checking

---
+++ build/precache-manifest.13b989413739d003258b49a6ca3219b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bffb6286ceb266515ec",
+    "url": "/clickme/static/js/runtime~main.7bffb628.js"
+  },
+  {
+    "revision": "b20815636f0a6a4221db",
+    "url": "/clickme/static/js/main.b2081563.chunk.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/clickme/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "b20815636f0a6a4221db",
+    "url": "/clickme/static/css/main.332bae7f.chunk.css"
+  },
+  {
+    "revision": "d862db7a5c67e3b65df5feb690fe7203",
+    "url": "/clickme/index.html"
+  }
+];
