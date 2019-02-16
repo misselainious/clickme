735a2a83140576132caeed04bb6631922c1d03a9
Elaine <elaine.cadman@gmail.com>
2019-02-15 20:14:23 -0800

    Updates

--- precache-manifest.92c43c80fe2dfd492e9a21b82c291264.js
+++ precache-manifest.92c43c80fe2dfd492e9a21b82c291264.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clickme/static/js/runtime~main.7bffb628.js"
   },
   {
-    "revision": "308235454f98725c6eda",
-    "url": "/clickme/static/js/main.30823545.chunk.js"
+    "revision": "a4ec750679d6df53d332",
+    "url": "/clickme/static/js/main.a4ec7506.chunk.js"
   },
   {
     "revision": "ba2021e67c053dfd7e1d",
     "url": "/clickme/static/js/2.ba2021e6.chunk.js"
   },
   {
-    "revision": "308235454f98725c6eda",
-    "url": "/clickme/static/css/main.f424c968.chunk.css"
+    "revision": "a4ec750679d6df53d332",
+    "url": "/clickme/static/css/main.e898898d.chunk.css"
   },
   {
-    "revision": "01230388194aa57e6c3ee2d0b2d0027d",
+    "revision": "6ea9cb3d61fc1db763d915b846124134",
     "url": "/clickme/index.html"
   }
 ];
